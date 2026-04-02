84f60e9dc584150124cda65f58c984679c3e4d33
MaminiainaZinah0803 <rakotoarisonzinah@gmail.com>
2026-04-02 22:51:26 +0300

    Create vecteur.h

---
+++ sommeDesDeuxVecteurs/vecteur.h
@@ -0,0 +1,4 @@
+int entier(char variable);
+void les_vecteurs(float vecteur[], int taille, char nom );
+void les_vecteurs(float vecteur[], int taille, char nom );
+void affichage(float v4[], int taille);
