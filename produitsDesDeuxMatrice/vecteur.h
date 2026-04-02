0fd9c16c092c95c6e37f4ebf8856cd64cd530df6
MaminiainaZinah0803 <rakotoarisonzinah@gmail.com>
2026-04-02 22:32:36 +0300

    Create vecteur.h

---
+++ produitsDesDeuxMatrice/vecteur.h
@@ -0,0 +1,3 @@
+int entier(int n);
+void saisie_matrice(int taille, float matrice[][taille], char nom);
+void produit_matrice(int taille, float matrice1[][taille], float matrice2[][taille], float matrice3[][taille]);
